9f3dda3d1e77eacd7eb65328c4fff0b0d4a15654
Chen <chenliang@ku.edu>
2020-07-31 11:03:14 -0500

    Add comments about variable order for script

--- middle.sh
+++ middle.sh
@@ -1,3 +1,8 @@
+#Add ability to set line number
 #Add ability to run on any file
 
-head -n 15 "$1" | tail -n 5
+# $1 is the filename
+# $2 is the number of lines to send to head
+# $3 is the number of lines to send to tail 
+
+head -n "$2" "$1" | tail -n "$3"
